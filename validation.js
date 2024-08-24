e7110f14db723d2992c55aa4953c2a22eb8ea83d
Alvaro Bernucci <alvarosenabernucci@hotmail.com>
2024-08-24 16:39:50 -0300

    add format number in inputs

--- validation.js
+++ validation.js
@@ -14,6 +14,27 @@ export default function initValidation(){
 // Submit button
   
 
+function formatNumber(event){
+  let numberArray = event.target.value.split('.');
+  if(numberArray[1]){
+    numberArray[1] = numberArray[1].slice(0,2);
+    let numeroFormatado = numberArray.join('.');
+    event.target.value = numeroFormatado;
+    if(event.target.id === 'term'){
+      event.target.value = numberArray[0];
+    }
+  }
+}
+
+
+
+inputNumber.forEach((input) => {
+  input.addEventListener('input',formatNumber)
+})
+
+
+
+
 // Input Validation
   function checkRequired(event){
     event.preventDefault();
